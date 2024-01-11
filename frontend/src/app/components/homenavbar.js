f32ead1286f73d98407b0f77c807f3aa66188395
LionelWilsonBI <lionel.wilson@bordeauxindex.com>
2024-01-11 16:47:20 +0000

    more build fixes

--- frontend/src/app/components/homenavbar.js
+++ frontend/src/app/components/homenavbar.js
@@ -3,6 +3,7 @@ import { Dialog } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import { useState } from "react";
 import Image from 'next/image'
+import Link from 'next/link'
 
 const navigation = [
   { name: "Try our BMR calculator", href: "/user/kitchen/bmrcalculator" },
@@ -21,14 +22,14 @@ export function HomeNavBar({props=navigation }) {
       aria-label="Global"
     >
       <div className="flex lg:flex-1">
-        <a href="/" className="-m-1.5 p-1.5">
+        <Link href="/" className="-m-1.5 p-1.5">
           <span className="sr-only">Your Company</span>
           <Image
             src="https://tailwindui.com/img/logos/mark.svg?color=amber&shade=300"
             alt=""
             height={32}
             width={47} />
-        </a>
+        </Link>
       </div>
       <div className="flex lg:hidden">
         <button
@@ -42,22 +43,26 @@ export function HomeNavBar({props=navigation }) {
       </div>
       <div className="hidden lg:flex lg:gap-x-12">
         {props.map((item) => (
-          <a
+          <Link
             key={item.name}
             href={item.href}
             className="text-sm font-semibold leading-6 text-gray-100"
           >
             {item.name}
-          </a>
+          </Link>
         ))}
       </div>
       <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-        <a
+        <ul>
+          <li>
+        <Link
           href="/user/login"
           className="text-sm font-semibold leading-6 text-amber-300"
         >
           Log in <span aria-hidden="true">&rarr;</span>
-        </a>
+        </Link>
+        </li>
+        </ul>
       </div>
     </nav>
     <Dialog
@@ -69,7 +74,7 @@ export function HomeNavBar({props=navigation }) {
         <div className="fixed inset-0 z-50" />
         <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-gray-900 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
-            <a href="#" className="-m-1.5 p-1.5">
+            <Link href="/" className="-m-1.5 p-1.5">
               <span className="sr-only">Your Company</span>
               <Image
                 className="h-8 w-auto"
@@ -78,7 +83,7 @@ export function HomeNavBar({props=navigation }) {
                 height={32}
                 width={47}
                  />
-            </a>
+            </Link>
             <button
               type="button"
               className="-m-2.5 rounded-md p-2.5 text-gray-100"
@@ -92,22 +97,22 @@ export function HomeNavBar({props=navigation }) {
             <div className="-my-6 divide-y divide-gray-500/10">
               <div className="space-y-2 py-6">
                 {props.map((item) => (
-                  <a
+                  <Link
                     key={item.name}
                     href={item.href}
                     className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-100 hover:bg-gray-50"
                   >
                     {item.name}
-                  </a>
+                  </Link>
                 ))}
               </div>
               <div className="py-6">
-                <a
+                <Link
                   href="/user/login"
                   className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-100 hover:bg-gray-50"
                 >
                   Log in
-                </a>
+                </Link>
               </div>
             </div>
           </div>
